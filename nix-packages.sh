672f1a48dd2fedc4186b373a72d368172ec26e4c
karlovsek <43514900+karlovsek@users.noreply.github.com>
2022-12-21 15:55:53 +0100

    Update nix-packages.sh

--- nix-packages.sh
+++ nix-packages.sh
@@ -1,21 +1,21 @@
 #install packages
 nix-env -iA \
-	nixpkgs.git \
-	nixpkgs.lazygit \
-	nixpkgs.tmux \
-	nixpkgs.stow \
-	nixpkgs.fzf \
-	nixpkgs.fasd \
-	nixpkgs.silver-searcher \
-	nixpkgs.ripgrep \
-	nixpkgs.bat \
-  	nixpkgs.exa \
-  	nixpkgs.fasd \
-  	nixpkgs.broot \
-  	nixpkgs.lazygit \	
-	nixpkgs.zsh \
-	nixpkgs.oh-my-zsh \
-	nixpkgs.zsh-powerlevel10k \
-	nixpkgs.fzf-zsh \
-  	nixpkgs.neovim \
-	nixpkgs.vim 
+        nixpkgs.git \
+        nixpkgs.lazygit \
+        nixpkgs.tmux \
+        nixpkgs.stow \
+        nixpkgs.fzf \
+        nixpkgs.fasd \
+        nixpkgs.silver-searcher \
+        nixpkgs.ripgrep \
+        nixpkgs.bat \
+        nixpkgs.exa \
+        nixpkgs.fasd \
+        nixpkgs.broot \
+        nixpkgs.lazygit \
+        nixpkgs.zsh \
+        nixpkgs.oh-my-zsh \
+        nixpkgs.zsh-powerlevel10k \
+        nixpkgs.fzf-zsh \
+        nixpkgs.neovim \
+        nixpkgs.vim
