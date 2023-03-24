d24f4420f557957ab73ace3c1370a06dbeed787e
Peter Karlovsek <peter.karlovsek@gmail.com>
2023-03-24 13:46:01 +0100

    Fix ordering

--- install.sh
+++ install.sh
@@ -3,10 +3,10 @@
 # install nix
 curl -L https://nixos.org/nix/install | sh
 
-source nix-packages.sh
-
 # source nix
 . ~/.nix-profile/etc/profile.d/nix.sh
+
+source nix-packages.sh
 
 echo "If not add this to ~/.zshrc for ZSH or ~/.profile for bash"
 echo "echo \"if [ -e ~/.nix-profile/etc/profile.d/nix.sh ]; then . ~/.nix-profile/etc/profile.d/nix.sh; fi\" >> ~/.zshrc"
